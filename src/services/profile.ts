497f923a8e2b065218075ee5c45d39c52c643d3c
JPSR-git <paulo.emailmehere@gmail.com>
2026-03-23 13:16:45 +0800

    refactor: remove unused profile photo service and related code

--- src/services/profile.ts
+++
@@ -1,8 +0,0 @@
-import { getProfileSettings } from "@/services/site-content";
-
-const DEFAULT_PROFILE_PHOTO = "/images/mecha-profile-photo.svg";
-
-export async function getProfilePhotoSrc(): Promise<string> {
-  const settings = await getProfileSettings();
-  return settings.profilePhotoSrc || DEFAULT_PROFILE_PHOTO;
-}
